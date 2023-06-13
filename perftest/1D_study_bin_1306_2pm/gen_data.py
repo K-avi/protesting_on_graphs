20f6c5fc01ca5f9f6147ead2c7c87a3396ae6933
ivan <i.m-r.ecole@orange.fr>
2023-06-13 14:06:15 +0200

    starting simul before going back to work on the actual code

---
+++ perftest/1D_study_bin_1306_2pm/gen_data.py
@@ -0,0 +1,27 @@
+#this file was written by https://github.com/Pacidus
+import numpy as np
+import subprocess as sb
+
+from time import time
+
+N = 30
+rmin = np.log10(100)
+rmax = np.log10(5000)
+while True:
+    r = int(10**(rmin + (np.random.rand(1)[0]*(rmax-rmin))))
+    print(r)
+
+    sb.run(f"python ../../graph_treatment/graph_generator.py 48.866 2.33 {r} 10 test.csv", shell=True)
+    with open("test.csv") as NTM:
+        fl = NTM.readline()
+
+    fl = float(fl.split(",")[0])
+    print(fl)
+    tb = time()
+    sb.run("wog13_06_cstwk_bin test.csv 1 1000", shell=True)
+    tf = time()
+
+    data = [fl, tf - tb]
+    print(data[1])
+    with open("data.csv", "a") as NTM:
+        np.savetxt(NTM, [data])

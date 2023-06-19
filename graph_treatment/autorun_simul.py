redid the whole graph dt analysis ; fixed minor stuff in tactics calculation

graph_treatment/autorun_simul.py
@@ -19,17 +19,17 @@ def gen_data_groups( nb_wk, t_curnum, adj, mobility_mean):
    
     for i in t_curnum:
         print("analysis for iteration : ", it)
-        print("pb gdg 0 ")
-        nadj = lt.merge_wknum_row_dictgraph(i, adj )
+      
+        nadj = lt.merge_wknum_adj_mat(i, adj )
         start = t.time()
         print("pb gdg 1 ")
-        group_array=dt.get_adj_group(i, nadj)
+        group_array=dt.get_adj_group_var(i, nadj)
         
-        nb_gp,adj_mat,diff = group_array
+        nb_gp,adj_mat = group_array
         print("adj group generated in", t.time()-start)
         
-        ret= np.append(ret , np.array([dt.count_groups(group_array) , dt.spreading_groups(group_array), \
-                    dt.get_mean_group_size( nb_wk , group_array), mobility_mean[it], diff]))
+        ret= np.append(ret , [nb_gp , dt.spreading_groups(group_array), \
+                    dt.get_mean_group_size( nb_wk , group_array), mobility_mean[it], 0])
         it+=1
         print("pb gdg 2")
         del(group_array)

graph_treatment/data_analysis.py
@@ -30,8 +30,32 @@ def get_adj_group(node_walker_num_arr, nadj):
    
     return n, np.array(n_labels) , len(labels)-nb_wk
 
+def get_adj_group_var(node_walker_num_arr, nadj):
+    """
+    np.array[1D]  , adj_matrix -> cg.sparse.csgraph
 
+    creates the subgraphs of graph
+    representing the groups of walkers
+    relies on the csgraph connected
+    componnents function
+    fast
+    """
+    N, labels = cg.connected_components(nadj, directed=False)
+    print("N before update", N)
+    uni, idx, inv, count = np.unique(labels, True, True, True)
 
+    # Generation of the new unique labels steadily increasing
+    # from 0 to n-1 the number of new graphs. and -1 for the removed nodes.
+    mask = (count != 1) * (node_walker_num_arr[idx] != 1)
+    Nun = (np.cumsum(mask) * mask) - 1
+
+    # reevaluate stuff
+    N = mask.sum()
+    print("mask, Nun", mask , Nun)
+    print("ms , nun.s", mask.sum(), (Nun == -1).sum())
+    print("num of con comp is : " ,N)
+    labels = Nun[inv]
+    return N, labels
 def count_groups(adj_mat):
     """
     cg sparce matrix -> num
@@ -41,7 +65,7 @@ def count_groups(adj_mat):
     
     O(1)
     """
-    a,b,n = adj_mat
+    a,b = adj_mat
     return a
 
 def spreading_groups(adj_mat): 
@@ -55,10 +79,10 @@ def spreading_groups(adj_mat):
     
     O(1)
     """
-    a,b,n = adj_mat
+    a,b = adj_mat
     v,c = np.unique(b , return_counts=True)
-
-    return sum(c)/len(c)
+    
+    return sum(c)/a
 
 
 def get_mean_group_size(nb_wk, adj_mat):
@@ -70,7 +94,7 @@ def get_mean_group_size(nb_wk, adj_mat):
     calculates the mean of the nb of 
     walkers present in the groups
     """
-    a,b,n = adj_mat
+    a,b = adj_mat
     return nb_wk/a
 
 

graph_treatment/plot_mean.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 
 plt.figure(1)
 
-dt_align = np.loadtxt("res_100_paris_align_var")
-dt_attra = np.loadtxt("res_100_paris_attra_var")
-dt_attco = np.loadtxt("res_100_paris_attra_coeff_var")
-dt_noisy_align = np.loadtxt("res_100_paris_noisy_align_var")
-dt_noisy_attra = np.loadtxt("res_100_paris_noisy_attra_var")
+dt_align = np.loadtxt("res_100_paris_align")
+dt_attra = np.loadtxt("res_100_paris_attra")
+dt_attco = np.loadtxt("res_100_paris_attra_coeff")
+dt_noisy_align = np.loadtxt("res_100_paris_noisy_align")
+dt_noisy_attra = np.loadtxt("res_100_paris_noisy_attra")
 
 plt.plot(dt_align.T[0], color='r', label='align')
 plt.plot(dt_attra.T[0], color='y', label='attra')